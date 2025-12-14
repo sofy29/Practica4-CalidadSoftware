0e7945197542485cf1c65fd75850159307f2b0cd
sofy29 <sofybr29@gmail.com>
2025-12-14 12:42:53 +0100

    Arreglo de errores en pruebas unitarias

--- src/musica/test/BibliotecaMusicalTest.java
+++ src/musica/test/BibliotecaMusicalTest.java
@@ -57,7 +57,7 @@ public class BibliotecaMusicalTest {
     @Test
     public void testAssertEquals_numCancionesPorArtista() {
         List<Cancion> resultado = biblioteca.buscarCancionPorArtista("Artist 1");
-        assertEquals("Debe haber 2 canciones de Artist 1", 700, resultado.size());
+        assertEquals("Debe haber 2 canciones de Artist 1", 2, resultado.size());
     }
 
     // 2) assertTrue
